9e9ecaafc36b340e165aadd75861b05404ed94cc
Vilo4ka <saiviolet@gmail.com>
2021-08-30 20:52:30 +0300

    открытие/закрытие окна, плавное появление, данные в инпут

--- pages/script.js
+++ pages/script.js
@@ -1,19 +1,30 @@
-const buttonEdit  = document.querySelector('.button_type_edit'); // получила кнопку редактирования
-const buttonClose = document.querySelector('.button_type_close-window'); //получила кнопку закрытия окна
+
+
+const profileButtonEdit  = document.querySelector('.button_type_edit'); // получила кнопку редактирования
+const profileButtonClose = document.querySelector('.button_type_close-window'); //получила кнопку закрытия окна
+const profilebuttonSave = document.querySelector('.button_type_save'); 
 let profileName = document.querySelector('.profile__name').textContent; //получила имя
 let profileActivity = document.querySelector('.profile__activity').textContent; //получила деятельность
 
 //функция открытия-закрытия окна
-function ModalWindow () {
-  const popup = document.querySelector('.popup');
+function profileModalWindow () {
+  const popup = document.querySelectorAll('.popup')[0];
   popup.classList.toggle('popup_opened');
-  let profileNameForm = document.querySelector('.form__item_element_profile-name');
-  profileNameForm.value = profileName;
-  let profileActivityForm = document.querySelector('.form__item_element_profile-activity');
-  profileActivityForm.value = profileActivity;
+  let nameInput = document.querySelector('.form__item_element_profile-name');
+  nameInput.value = profileName;
+  let ActivityInput = document.querySelector('.form__item_element_profile-activity');
+  ActivityInput.value = profileActivity;
 }
 
+//функция открытия-закрытия окна
+function cardsModalWindow () {
+  const popup = document.querySelectorAll('.popup')[1];
+  popup.classList.toggle('popup_opened');
+  let nameInput = document.querySelector('.form__item_element_profile-name');
+  nameInput.value = profileName;
+  let ActivityInput = document.querySelector('.form__item_element_profile-activity');
+  ActivityInput.value = profileActivity;
+}
 
-
-buttonEdit.addEventListener('click', ModalWindow);
-buttonClose.addEventListener('click', ModalWindow);
+profileButtonEdit.addEventListener('click', profileModalWindow);
+profileButtonClose.addEventListener('click', profileModalWindow);
