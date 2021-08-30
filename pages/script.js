a9f1947f19a1ba93c91c5f6ea02e5164ceb3d073
Vilo4ka <87121146+saiviolet@users.noreply.github.com>
2021-08-30 17:48:09 +0300

    доделала окна

--- pages/script.js
+++ pages/script.js
@@ -2,10 +2,11 @@ let profileName = document.querySelector('.profile__name').textContent; //пол
 let profileActivity = document.querySelector('.profile__activity').textContent; //получила деятельность
 const buttonProfileEdit = document.querySelector('.button_type_edit'), //получила кнопку редактирования
 buttonCardsAdd = document.querySelector('.button_type_add'), //получила кнопку редактирования
-popups = document.querySelectorAll('.popup') //выбрала все модальные окна
+popups = document.querySelectorAll('.popup'), //выбрала все модальные окна
+buttonsClose = document.querySelectorAll('.button_type_close-window'), //получила кнопки закрытия окон
 profileEditPopup = popups[0], // определила окно редактирования профайла
-cardsAddPopup = popups[1], // определила окно редактирования профайла
-buttonClose = document.querySelector('.button_type_close-window'); //получила кнопку закрытия окна
+cardsAddPopup = popups[1]; // определила окно редактирования профайла
+const cardsList = document.querySelector('.cards__items');
 //функция открытия окна
 function openPopup(popup) {
   popup.classList.add('popup_opened');
@@ -21,7 +22,7 @@ buttonProfileEdit.addEventListener('click', () => {
   profileNameForm.value = profileName; //вывод имени
   let profileActivityForm = document.querySelector('.form__item_element_profile-activity');
   profileActivityForm.value = profileActivity; //вывод рода деятельности
-  buttonClose.addEventListener('click', () => { //функция закрытия окна
+  buttonsClose[0].addEventListener('click', () => { //функция закрытия окна
     closePopup(profileEditPopup);
   });
 });
@@ -29,7 +30,18 @@ buttonProfileEdit.addEventListener('click', () => {
 //ообработка клика на кнопку добавления карточки
 buttonCardsAdd.addEventListener('click', () => {
   openPopup(cardsAddPopup); //вызов ф-ии
-  buttonClose.addEventListener('click', () => { //функция закрытия окна
-    closePopup(profileEditPopup);
+  buttonsClose[1].addEventListener('click', () => { //функция закрытия окна
+    closePopup(cardsAddPopup);
   });
 });
+
+//заполнение карточками
+function addCard(cardName, cardurl) {
+  const cardTemplate = document.querySelector('#card').content;
+  const cardElement = cardTemplate.querySelector('.cards__item').cloneNode(true);
+  cardElement.querySelector('.cards__item-name').textContent = cardName;
+  cardElement.querySelector('.cards__image').src = cardurl;
+  cardsList.append(cardElement);
+}
+let c = 'fdfdfd', b = 'images/gallery/1-castle-combe.jpeg';
+addCard(c, b);
