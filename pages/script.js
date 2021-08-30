69b66e94f5d6827324391dff2e1db05c0aeec41d
Vilo4ka <saiviolet@gmail.com>
2021-08-30 21:18:06 +0300

    открытие/закрытие окна, плавное появление, данные в инпут

--- pages/script.js
+++ pages/script.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-
-
-const profileButtonEdit  = document.querySelector('.button_type_edit'); // получила кнопку редактирования
-const profileButtonClose = document.querySelector('.button_type_close-window'); //получила кнопку закрытия окна
-const profilebuttonSave = document.querySelector('.button_type_save'); 
-let profileName = document.querySelector('.profile__name').textContent; //получила имя
-let profileActivity = document.querySelector('.profile__activity').textContent; //получила деятельность
-
-//функция открытия-закрытия окна
-function profileModalWindow () {
-  const popup = document.querySelectorAll('.popup')[0];
-  popup.classList.toggle('popup_opened');
-  let nameInput = document.querySelector('.form__item_element_profile-name');
-  nameInput.value = profileName;
-  let ActivityInput = document.querySelector('.form__item_element_profile-activity');
-  ActivityInput.value = profileActivity;
-}
-
-//функция открытия-закрытия окна
-function cardsModalWindow () {
-  const popup = document.querySelectorAll('.popup')[1];
-  popup.classList.toggle('popup_opened');
-  let nameInput = document.querySelector('.form__item_element_profile-name');
-  nameInput.value = profileName;
-  let ActivityInput = document.querySelector('.form__item_element_profile-activity');
-  ActivityInput.value = profileActivity;
-}
-
-profileButtonEdit.addEventListener('click', profileModalWindow);
-profileButtonClose.addEventListener('click', profileModalWindow);
-=======
 let profileName = document.querySelector('.profile__name').textContent; //получила имя
 let profileActivity = document.querySelector('.profile__activity').textContent; //получила деятельность
 const buttonProfileEdit = document.querySelector('.button_type_edit'), //получила кнопку редактирования
@@ -77,4 +45,4 @@ function addCard(cardName, cardurl) {
 }
 let c = 'fdfdfd', b = 'images/gallery/1-castle-combe.jpeg';
 addCard(c, b);
->>>>>>> a9f1947f19a1ba93c91c5f6ea02e5164ceb3d073
+
