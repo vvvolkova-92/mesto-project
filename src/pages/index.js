40df1f1a2a346af01d831f6079baeb19f9cc8adc
Vilo4ka <saiviolet@gmail.com>
2021-11-15 19:05:19 +0300

    исправлены все комменатрии ревььера

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,14 @@
 import css from './index.css'
 import './index.css'
 import {formProfileEdit,nameInput, activityInput, profileName, profileActivity, profileAvatar, cardsList, buttonProfileEdit, 
-  buttonCardsAdd, buttonProfilePhotoEdit, cardsAddPopup, allPopups, profileEditPhotoPopup,editPhotoButton} from '../components/constants.js'
+  buttonCardsAdd, buttonProfilePhotoEdit, cardsAddPopup, allPopups, profileEditPhotoPopup,editPhotoButton, config} from '../components/constants.js'
 import {enableValidation} from '../components/validate.js'
 import {getUserData, getInitialCards} from '../components/api.js'
 import {createCard} from '../components/card.js'
 import {openPopup, closePopup, openProfilePopup} from '../components/modal.js'
 export let userId;
 //кнопки
-const config = {
-  inputSelector: '.form__item',
-  buttonSelector: '.button_type_save',
-  errorClass: 'form__item_error',
-};
+
 
 Promise.all([getUserData(), getInitialCards()])
   .then(([userData, cardData]) => {
@@ -64,20 +60,3 @@ buttonCardsAdd.addEventListener('click', () => {
 
 
 enableValidation(config);
-// getUserData()
-//   .then(result => {
-//     profileName.textContent = result.name //вывод имени
-//     profileActivity.textContent = result.about //вывод рода деятельности
-//     profileAvatar.src = result.avatar //аватар
-//     const submitButton = formProfileEdit.querySelector('.button_type_save');
-//     submitButton.disabled = false;
-//   })
-//   .catch(err => console.log(`Ошибочка вышла: ${err} - ${err.status}`))
-// getInitialCards()
-//   .then(card => {
-//     card.forEach(cardData => {
-//       const card = createCard(cardData);
-//       cardsList.append(card);
-//     })
-//   })
-//   .catch(err => console.log(`Ошибочка вышла: ${err}`))
